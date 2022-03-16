d0b433bc6ea370f46d74de25b3685764d2fc24d7
Cam Ellis <elliscj@colorado.edu>
2022-03-16 12:23:49 -0600

    uncommitted changes

--- src/components/FeaturedSongs.js
+++ src/components/FeaturedSongs.js
@@ -1,1 +1,5 @@
+const FeaturedSongs = () => {
+  return <h2>embedded fearured songs from soundcloud</h2>;
+};
 
+export default FeaturedSongs;
--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,5 +1,12 @@
+import FeaturedSongs from "../components/FeaturedSongs";
+
 const Music = () => {
-  return <h1>this is where the music goes</h1>;
+  return (
+    <>
+      <h1>this is where the music goes</h1>
+      <FeaturedSongs />
+    </>
+  );
 };
 
 export default Music;
